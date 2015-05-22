3a2fb6e1133e392ed6c78fd18831eeb53ce6139b
Markus Mayer <development@widemeadows.de>
2015-05-22 20:03:59 +0200

    added trivial Math.Net example

--- Neural/Program.cs
+++ Neural/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Single;
 
 namespace Neural
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Matrix<float> A = DenseMatrix.OfArray(new float[,]
+                                                  {
+                                                      {1, 1, 1, 1},
+                                                      {1, 2, 3, 4},
+                                                      {4, 3, 2, 1}
+                                                  });
+
+            Matrix<float> B = DenseMatrix.OfArray(new float[,]
+                                                  {
+                                                      {1, 1, 1, 1},
+                                                      {1, 2, 3, 4},
+                                                      {4, 3, 2, 1}
+                                                  });
+
+            var C = A + B;
         }
     }
 }
