103472ae5c4f6cea911d7be5c81c157d8ec1e6ca
Markus Mayer <development@widemeadows.de>
2015-05-22 20:22:56 +0200

    started with rough perceptron network definitions

---
+++ Neural/perceptron/Layer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Neural.perceptron
+{
+    /// <summary>
+    /// A network layer consisting of multiple <see cref="Perceptron"/> instances.
+    /// </summary>
+    class Layer
+    {
+        /// <summary>
+        /// The perceptrons in this layer
+        /// </summary>
+        private readonly ICollection<Perceptron> _perceptrons;
+
+        /// <summary>
+        /// Gets the count.
+        /// </summary>
+        /// <value>The count.</value>
+        public int Count
+        {
+            get { return _perceptrons.Count; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Layer"/> class.
+        /// </summary>
+        /// <param name="perceptrons">The perceptrons.</param>
+        public Layer(ICollection<Perceptron> perceptrons)
+        {
+            _perceptrons = perceptrons;
+        }
+    }
+}
---
+++ Neural/perceptron/Perceptron.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural.perceptron
+{
+    /// <summary>
+    /// A perceptron.
+    /// </summary>
+    class Perceptron
+    {
+        /// <summary>
+        /// The activation function
+        /// </summary>
+        private readonly Activation _activation;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Perceptron"/> class.
+        /// </summary>
+        /// <param name="activation">The activation function.</param>
+        public Perceptron(Activation activation)
+        {
+            _activation = activation;
+        }
+
+        /// <summary>
+        /// Calculates the activation of the perceptron.
+        /// </summary>
+        /// <returns>System.Single.</returns>
+        public float Activate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
