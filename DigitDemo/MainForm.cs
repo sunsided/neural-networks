74e0dab31f73c32dd209e1a06b311d033a9e4f8b
Markus Mayer <development@widemeadows.de>
2015-05-31 04:33:37 +0200

    started digit demo program

---
+++ DigitDemo/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Widemeadows.MachineLearning.Neural.Demonstration.Digit
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
